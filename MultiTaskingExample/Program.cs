e5a5ab00df10f41f2645c500a94561546f234a79
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2023-05-18 12:02:36 +0530

    Async and Await

---
+++ MultiTaskingExample/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Method1();
+        Method2();
+        Console.ReadKey();
+    }
+
+    public static async Task Method1()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(" Method 1 " + i);
+                // Do something
+                Task.Delay(3000);
+            }
+        });
+    }
+
+
+    public static async void Method2()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("---------------------------------------- Method 2 -- " + i);
+                // Do something
+                Task.Delay(1000);
+            }
+
+        });
+    }
+}
