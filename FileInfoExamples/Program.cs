6a49098cea546a1cf650307f217d1458b1a95cb7
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2023-05-19 12:14:58 +0530

    nullable ex

---
+++ FileInfoExamples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+ File.Create("Suresh.txt");
+if (File.Exists("Suresh.txt"))
+{
+    Console.WriteLine("File Created");
+}
+else
+{
+    Console.WriteLine("File modified");
+}
+
+var process = from p in System.Diagnostics.Process.GetProcesses()
+              select p.ProcessName;
+
+// Write file using StreamWriter
+using (StreamWriter writer = new StreamWriter(Path.GetFullPath("Suresh.text")))
+{
+    foreach (var item in process)
+    {
+        writer.WriteLine(item);
+    }
+}
+// Read a file
+string readText = File.ReadAllText(Path.GetFullPath("Suresh.text"));
+Console.WriteLine(readText);
+
+string fileName = Path.GetFullPath("Suresh.text");
+FileInfo fi = new FileInfo(fileName);
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ MiscEx/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MiscEx;
+
+Console.WriteLine("Hello, World!");
+int? x = null;
+string emp = null;
+int? empno = null;
+
+Class1 cls = null;
+
+if (cls != null)
+    Console.WriteLine(cls.MyProperty);
+
+if (emp is null)
+{
+
+}
+
+emp = null;
+emp = string.Empty;
+if (string.IsNullOrEmpty(emp))
+{
+
+}
+
+//3 dept
+
+//1 d1
+//2 d2
+//3 d3
+
+//5 employy
+
+//    1 e1 d1
+//    2 e2 d2
+//    3 e3 d1
+//    4 e4 d1
+//    5 e5 d2
+
+//    d1 e1
+//    d1 e3
+//    d1 e4
+//    d2 e2
+//    d2 e5
+//    d3 null
